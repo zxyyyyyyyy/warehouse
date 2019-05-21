249bd6f5dee1139e8f897e298167edf81c181d11
zxyyyyyyyy <1849403716@qq.com>
2019-05-21 18:55:28 +0800

    admin

---
+++ wareh/react-app1/src/conmponent/C.js
@@ -0,0 +1,11 @@
+import React,{ Component } from "react"
+
+let C = (props) => {
+    console.log(props.match.params.num)
+    return (
+        <div>
+            CCCCCCC
+        </div>
+    )
+}
+export default C;
