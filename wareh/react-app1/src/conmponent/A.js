0434c26876c57d751d2671dffa6a43c70ab3dbc5
zxyyyyyyyy <1849403716@qq.com>
2019-05-21 18:55:14 +0800

    admin

---
+++ wareh/react-app1/src/conmponent/A.js
@@ -0,0 +1,12 @@
+import React,{ Component } from "react"
+
+export default class A extends Component{
+    render(){
+        console.log(this.props.match.params.id);
+        return(
+            <div>
+                AAAAAAAA
+            </div>
+        )
+    }
+}
