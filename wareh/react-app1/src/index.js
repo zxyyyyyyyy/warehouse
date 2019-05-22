4a1e4387288b66df04cb888828daf64e72714f2c
zxyyyyyyyy <1849403716@qq.com>
2019-05-22 15:01:53 +0800

    admin

--- wareh/react-app1/src/index.js
+++ wareh/react-app1/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
+import store from './store'
+//store用来存储状态的
+ReactDOM.render(
+<Provider store={store}>
+<App />
+
+</Provider>
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+, document.getElementById('root'));
 
 
