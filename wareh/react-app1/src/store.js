4b90e892662d287b0dca3425e0dde52d4aa7e346
zxyyyyyyyy <1849403716@qq.com>
2019-05-23 19:21:16 +0800

    admin

--- wareh/react-app1/src/store.js
+++ wareh/react-app1/src/store.js
@@ -1,9 +1,10 @@
-import {creatStore} from 'redux'
-//creatStore创建store  第一个参数是reducer  第二个参数初始化状态
+import { createStore } from 'redux'
+import reducer from './reducer'
+//createStore创建store  第一个参数是reducer  第二个参数初始化状态
 let initialState = {
     num:0,
 }
-let store = creatStore(()=>{
+let store = createStore(()=>{
 
 },initialState);
 export default store;
