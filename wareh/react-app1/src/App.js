c664603139037e1d2ee8eafb7905349d5212f3b8
zxyyyyyyyy <1849403716@qq.com>
2019-05-23 19:20:48 +0800

    admin

--- wareh/react-app1/src/App.js
+++ wareh/react-app1/src/App.js
@@ -8,12 +8,28 @@ import A from "./conmponent/A"
 import B from "./conmponent/B"
 import C from "./conmponent/C"
 import Children from "./conmponent/Children"
-
+import store from './store'
+import {add,reduce} from './action'
+import {connect} from ''
 import Home from "./conmponent/Home"
-function App() {
+function App(props) {
   return (
     <div className="App">
-      HAAH
+      <div>{props}</div>
+      <div> HAAH</div>
+      <button onClick={()=>{
+        console.log('我要修改Num值');
+        //通过store.dispatch触发action修改num
+        store.dispatch(add(2));
+      }}>add</button>
+<button onClick={()=>{
+        console.log('我要修改Num值');
+        //通过store.dispatch触发action修改num
+        store.dispatch(reduce());
+      }}>reduce</button>
+
+
+      <hr></hr>
      <Router>
        <Link to="/">首页面</Link>
       ------
@@ -36,24 +52,28 @@ function App() {
        <Route path="/b" component={B}></Route> */}
          
 
-       <Route exact path="/" component={Home}></Route>
+       {/* <Route exact path="/" component={Home}></Route>
        <Route exact path="/a/:id" component={A}></Route>
        <Route exact path="/b" component={B}></Route>
        <Route exact path="/c/:num" component={C}></Route>
        
        <Route exact path="/d" render={() =>{
          return <h1>render   dddddd</h1>
-       }}></Route>
-        <Route path="/children" children={(props)=>{
+       }}></Route> */}
+        {/* <Route path="/children" children={(props)=>{
           let str = props.match?"1111":"0000";
           console.log(str);
 
           return <Children str={str}></Children>
         }}></Route>
-       
+        */}
      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
+let mapStateToProps =(state) =>{
+  return {
+    num:state.num
+  }
+}
+export default connect(mapStateToProps)(App);
