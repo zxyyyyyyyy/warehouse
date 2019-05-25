bd477668496044687d11928799dddaa0a6188720
zxyyyyyyyy <1849403716@qq.com>
2019-05-25 14:38:51 +0800

    admin

---
+++ wareh/antd-demo/component/NavLeft/index.js
@@ -0,0 +1,45 @@
+import React ,{Component } from 'react'
+import {Menu,Icon} from 'antd';
+import menuList from './../../config/menuconfig'
+const {SubMenu} = Menu;
+
+export default  class NavLeft extends Component{
+    createMenu = (menuList) => {
+        let List = menuList.map((elem) =>{
+            if(elem.children){
+                return(
+                    <SubMenu title={elem.title}>
+                        {/* 递归调用  循环生成子菜单 */}
+                        {this.createMenu(elem.children)}
+                    </SubMenu>
+                )
+            }
+            return (
+                <Menu.Item key={elem.path}>
+                    <Icon type={elem.icon}></Icon>
+                    {elem.title}
+                </Menu.Item>
+            )
+        })
+        this.setState({
+            List
+        })
+    }
+    componentWillMount = () =>{
+        //取到map循环出来的数组
+        let list = this.createMenu(menuList);
+        this.setState({
+            list
+        })
+        
+    }
+    render(){
+        return(
+            <div>
+                <Menu theme="dark" mode="inline">
+                    {this.state.list}
+                </Menu>
+            </div>
+        )
+    }
+}
