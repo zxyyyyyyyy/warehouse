824c6e551795860d3d79a02de880fbfec93730ea
zxyyyyyyyy <1849403716@qq.com>
2019-05-27 19:49:50 +0800

    admin

--- wareh/antd-demo/src/App.js
+++ wareh/antd-demo/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Button } from 'antd';
 import './App.css';
 import {
  
@@ -9,12 +7,15 @@ import {
 
 } from "antd"
 import NavLeft from './component/NavLeft'
+import CHeader from './component/CHeader'
+import CFooter from './component/CFooter'
+import Home from './page/Home/index'
 
 function App() {
   return (
     <div className="App">
       <Row>
-        <Col span={4} >
+        <Col span={4} className="navleft">
           <NavLeft></NavLeft>
         </Col>
         <Col span={20}>right</Col>
