dbe7324e6bc501b7a1dacc4feff45b2dce67e90d
zxyyyyyyyy <1849403716@qq.com>
2020-03-15 16:30:09 +0800

    test

--- wareh/react-app1/src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore ,applyMiddleware} from 'redux'
-import reducer from './reducer'
-import thunk from 'redux-thunk'
-//createStore创建store  第一个参数是reducer  第二个参数初始化状态
-let initialState = {
-    num:0,
-}
-let store = createStore(reducer,createStore,applyMiddleware(thunk));
-export default store;
